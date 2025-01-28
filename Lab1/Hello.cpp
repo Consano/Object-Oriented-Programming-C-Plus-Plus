07a649e7c30f415a33255ffc1ec0a31b6aff4851
Consano <jackkeefer@gmail.com>
2025-01-28 14:32:58 -0800

    Create Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/**
+ *  Simple hello world program to test environment and github commits
+ */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
